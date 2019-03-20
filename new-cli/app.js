27d0a155f32aa72807ff53172813e3cf8f9a1b8e
BraulioRuiz <43392482+BraulioRuiz@users.noreply.github.com>
2019-03-20 11:55:45 -0600

    Update app.js

--- new-cli/app.js
+++ new-cli/app.js
@@ -28,9 +28,9 @@ inquirer.prompt([
                 value: '1'
             },
             {
-                name: 'whois',
+                name: 'Cron',
                 type: 'checkbox',
-                message: 'Who is',
+                message: 'Cron',
                 value: '2'
             },
             {
@@ -51,7 +51,7 @@ inquirer.prompt([
     console.log(answers);
     switch(answers.list){
         case '1':
-            pingShell()
+            cronShell()
           break;
         case '2':
             whoisShell()
@@ -60,12 +60,79 @@ inquirer.prompt([
             wgetShell()
           break;
         default:
-          // code block
+           //code block
       } 
-    //  if (answers.list == 1){
-    //     pingShell()
-    // }
+    //   if (answers.list == 1){
+    //      pingShell()
+    //  }
 });
+
+function cronShell(){
+    var cron;
+    inquirer.prompt([
+        {
+            name: 'minuto',
+            type: 'input',
+            message: 'A que minuto desea programarlo? ',  
+            validate: function validateFirstName(name){
+                return name !== '';
+            }          
+        },
+        {
+            name: 'hora',
+            type: 'input',
+            message: 'A que hora desea programarlo? ',  
+            validate: function validateFirstName(name){
+                return name !== '';
+            }          
+        },
+        {
+            name: 'dia',
+            type: 'input',
+            message: 'A que dia del mes desea programarlo? ',  
+            validate: function validateFirstName(name){
+                return name !== '';
+            }          
+        },
+        {
+            name: 'mes',
+            type: 'input',
+            message: 'A que mes del año desea programarlo? ',  
+            validate: function validateFirstName(name){
+                return name !== '';
+            }          
+        },
+        {
+            name: 'diaSemana',
+            type: 'input',
+            message: 'A que dia de la semana desea programarlo? ',  
+            validate: function validateFirstName(name){
+                return name !== '';
+            }          
+        },
+        {
+            name: 'ejecutar',
+            type: 'input',
+            message: 'Que comando desea ejecutar? ',  
+            validate: function validateFirstName(name){
+                return name !== '';
+            }          
+        },
+        {
+            name: 'direccion',
+            type: 'input',
+            message: 'Escriba la direccion del programa a ejecutar ',  
+            validate: function validateFirstName(name){
+                return name !== '';
+            }          
+        }
+    ]).then(answer=>{        
+        cron = answer;
+        //console.log(cron);        
+        console.log( answer.minuto + ' ' + answer.hora + ' ' + answer.mes + ' ' + answer.diaSemana + ' ' +
+        answer.ejecutar + ' ' + answer.direccion);
+    });    
+}
 
 function pingShell(){
     console.log('Ping')
